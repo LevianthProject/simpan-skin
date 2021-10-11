9cb47fa3dfd21de39b592634adfa96be7cdc5dce
Atang Sutisna <atang@bippo.co.id>
2021-10-11 01:53:48 +0000

    move to global footer

--- dataSetAwal.php
+++ dataSetAwal.php
@@ -137,53 +137,11 @@
     </div>
     <!-- end:cardItems -->
 
-    <!-- footer -->
-    <footer
-      class="container-fluid background-1 footer-height footer-sm-height mt-5"
-    >
-      <div class="container-fluid">
-        <div
-          class="row flex-row pt-xs-1 pt-lg-2 d-flex font-poppins text-white"
-        >
-          <div
-            class="col ml-lg-5 pt-lg-4 d-flex flex-lg-row flex-xs-column flex-sm-column justify-content-lg-end"
-          >
-            <h3 class="font-size-lg-1 py-lg-0 py-xs-2 font-size-sm-6 mr-xl-3">
-              @Copyright 2021
-            </h3>
-            <h3
-              class="ml-lg-5 font-size-lg-1 py-lg-0 py-xs-2 font-size-sm-6 mr-xl-3"
-            >
-              <span class="mr-xl-3">|</span>
-              Aplikasi monitoring
-            </h3>
-            <h3 class="ml-lg-5 font-size-lg-1 py-lg-0 py-xs-2 font-size-sm-6">
-              <span class="mr-xl-3">|</span>
-              All Right Reserved
-            </h3>
-          </div>
-          <div
-            class="col pt-lg-4 d-flex flex-lg-row flex-xs-column flex-sm-column justify-content-lg-end pr-lg-5"
-          >
-            <h3 class="font-size-lg-1 font-size-sm-6 mr-lg-5">Follow Us</h3>
-            <div class="flex-row pt-xs-2 p-lg-0">
-              <i
-                class="fab fa-facebook-f facebook-color font-size-5 mr-lg-5 mr-xs-1"
-              ></i>
-              <i
-                class="fab fa-twitter twitter-color font-size-5 mr-lg-5 mr-xs-1"
-              ></i>
-              <i
-                class="fab fa-instagram instagram-color font-size-5 mr-lg-5 mr-xs-1"
-              ></i>
-            </div>
-          </div>
-        </div>
-      </div>
-    </footer>
+	<!-- footer -->
+	<?php include "global-footer.php" ?>
     <!-- end:footer -->
 
-    <script
+	<script
       src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
       integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
       crossorigin="anonymous"
